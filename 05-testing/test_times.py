46211b62cd9cd965217261b4a88eb9708e5091d0
Max Hipperson <40496579+maxhipperson@users.noreply.github.com>
2019-10-31 10:55:47 +0000

    Added test file Answers UCL-RITS/rsd-classwork-2019#18

---
+++ 05-testing/test_times.py
@@ -0,0 +1,15 @@
+from times import time_range, overlap_time
+
+def test_given_input():
+    large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
+    short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 3, 60)
+
+    # print(overlap_time(large, short))
+
+    result = overlap_time(large, short)
+    expected = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
+    assert result == expected
+
+if __name__ == "__main__":
+
+    test_given_input()
