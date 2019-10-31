42567aa9ad2bc9cd55ca389f75223f0fee259991
Max Hipperson <40496579+maxhipperson@users.noreply.github.com>
2019-10-31 11:28:05 +0000

    Next part Answers UCL-RITS/rsd-classwork-2019#25

--- 05-testing/test_times.py
+++ 05-testing/test_times.py
@@ -3,12 +3,18 @@ from times import time_range, overlap_time
 def test_given_input():
     large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
     short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
-
-    # print(overlap_time(large, short))
-
     result = overlap_time(large, short)
+
     expected = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
     assert result == expected
+
+def test_class_time():
+    obs1 = time_range("2019-10-31 10:00:00", "2019-10-31 13:00:00")
+    obs2 = time_range("2019-10-31 10:05:00", "2019-10-31 12:55:00", 3, 600)
+    result = overlap_time(obs1, obs2)
+
+    # How do you make an assertion that covers this without explicitly checking it?
+    assert result == obs2
 
 if __name__ == "__main__":
     test_given_input()
